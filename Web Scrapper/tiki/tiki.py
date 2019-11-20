d18419f6fcb14d649d06a36a3f88c772bd761e2d
TienTruong98 <ironlegionrocks@gmail.com>
2019-11-20 10:46:47 +0700

    Update tiki.py

--- Web Scrapper/tiki/tiki.py
+++ Web Scrapper/tiki/tiki.py
@@ -1,6 +1,5 @@
 import bs4
 import requests
-import csv
 import logging
 import pymongo
 import time
@@ -36,7 +35,7 @@ class DataPipeline:
         try:
             collection = DataPipeline.database[collection_name]
             for x in data:
-                if collection.find_one({'link': {"$eq": x['link']}}) == None:
+                if collection.find_one({'link': {"$eq": x['link']}}) is None:
                     collection.insert_one(x)
             logging.debug('Import data to collection "{}"'.format(collection_name))
         except Exception:
@@ -61,6 +60,7 @@ class Category:
         category_link_list = []
         category_name = []
         print('Getting category link.....')
+        logging.debug('Get Category link list')
         try:
             res = requests.get("https://tiki.vn/")
             soup = bs4.BeautifulSoup(res.text, 'lxml')
@@ -129,111 +129,127 @@ class SubCategory:
         # generate all the sub pages
         for i in range(1, count + 1):
             new_link = link + 'page=' + str(i)
-            sub_page_link_list.append({'name': name, 'page': i, 'link': new_link, 'status':True})
+            sub_page_link_list.append({'name': name, 'page': i, 'link': new_link, 'status': True})
         print('Finished getting sub category of {}'.format(name))
         return sub_page_link_list
 
     @staticmethod
     def iteratingCategoryLinks():
+        '''
+        iterating through the 'Category links' collection and making the relevant sub links
+        and export to 'Sub links' collection
+        :return:
+        '''
         for x in DataPipeline.importData('Category links'):
+            logging.debug('Scraping "{}"'.format(x['name']))
             if x['status']:
                 DataPipeline.exportData('Sub links', SubCategory.getCategorySubPage(x['name'], x['link']))
-                logging.info('Finished scraping "{}"'.format(x['name']))
                 DataPipeline.updateStatus('Category links', x['link'], False)
             else:
                 print('"{}" is already exist'.format(x['name']))
-                logging.info('"{}" is already exist'.format(x['name']))
-        logging.info('Finished scrapping sub category links')
+            logging.debug('Finished scraping "{}"'.format(x['name']))
+        logging.debug('Finished scrapping sub category links')
 
 
-def getItemID(soup):
-    '''
-    getting item's id
-    :param soup:
-    :return:
-    '''
-    id_list = []
-    script = ""
-    for context in soup.find_all("script"):
-        if "ga('ec:addImpression'," in context.text:
-            script = context.text.strip()
-    script = script.split("ga('ec:addImpression', {")
-    script.pop(0)
-    for i in range(len(script)):
-        id = script[i]
-        id = id.strip()
-        id = id.split('\n')
-        id = int(id[0][6:-2])
-        id_list.append(id)
-    return id_list
+class Item:
+    @staticmethod
+    def getItemID(soup):
+        '''
+        getting ID of an item
+        :param soup: the page source
+        :return: id_list: list of items ID
+        '''
+        id_list = []
+        id_script = ""
+        for context in soup.find_all("script"):
+            if "ga('ec:addImpression'," in context.text:
+                id_script = context.text.strip()
+        id_script = id_script.split("ga('ec:addImpression', {")
+        id_script.pop(0)
+        for i in range(len(id_script)):
+            id = id_script[i]
+            id = id.strip()
+            id = id.split('\n')
+            id = int(id[0][6:-2])
+            id_list.append(id)
+        return id_list
 
+    @staticmethod
+    def getInfoItem(link):
+        '''
+        get information of an item. That includes name, brand, price, rating of the item
+        :param link: the page link
+        :return: dictionary of items information
+        '''
+        try:
+            res = requests.get(link)
+            soup = bs4.BeautifulSoup(res.text, 'lxml')
 
-def getInfoItem(link):
-    try:
-        res = requests.get(link)
-        soup = bs4.BeautifulSoup(res.text, 'lxml')
+            name = soup.find("h1")
+            name = name.text.strip()
 
-        name = soup.find("h1")
-        name = name.text.strip()
+            brand = soup.find("div", class_="item-brand")
+            brand = brand.find('p')
+            brand = brand.text.strip()
 
-        brand = soup.find("div", class_="item-brand")
-        brand = brand.find('p')
-        brand = brand.text.strip()
+            price = soup.find("span", id="span-price")
+            price = price.text
 
-        price = soup.find("span", id="span-price")
-        price = price.text
+            rating = soup.find("meta", attrs={"itemprop": "ratingValue"})
+            rating = rating['content']
+            logging.debug('Get {}'.format(name))
+            return {'name': name, 'brand': brand, 'price': price, 'rating': rating, 'link': link}
+        except:
+            return {'name': '', 'brand': '', 'price': '', 'rating': '', 'link': link}
 
-        rating = soup.find("meta", attrs={"itemprop": "ratingValue"})
-        rating = rating['content']
-        return [name, brand, price, rating, link]
-    except:
-        return ['', '', '', '', link]
+    @staticmethod
+    def getItem(link):
+        '''
+        this function will first scrap the page and find all items ID
+        second it will find the link corresponding to that item ID and scrap the item's information
+        :param link: the menu page link
+        :return: items_info_list f
+        '''
+        items_info_list = []
+        try:
+            res = requests.get(link)
+            soup = bs4.BeautifulSoup(res.text, 'lxml')
+            # get items ID
+            item_IDs = Item.getItemID(soup)
+            for id in item_IDs:
+                item_link = soup.find('a', attrs={"data-id": id})['href']
+                # get item's information
+                item_information = Item.getInfoItem(item_link)
+                items_info_list.append(item_information)
+        except Exception as e:
+            print(e)
+        return items_info_list
 
-
-def getItem(link):
-    current_page_item_link_list = []
-    try:
-        res = requests.get(link)
-        soup = bs4.BeautifulSoup(res.text, 'lxml')
-        # get id of current page's items
-        item_id = getItemID(soup)
-        for id in item_id:
-            item_link = soup.find('a', attrs={"data-id": id})['href']
-            item_information = getInfoItem(item_link)
-            current_page_item_link_list.append(item_information)
-    except Exception as e:
-        print(e)
-    return current_page_item_link_list
-
-
-def getFileName():
-    with open('./tiki/category.txt', mode='r', encoding='utf-8-sig') as file:
-        lines = file.readlines()
-    category_name = []
-    for i in range(0, len(lines), 2):
-        category_name.append(lines[i].strip())
-    return category_name
-
-
-def createCSV():
-    category_name = getFileName()
-    for name in category_name:
-        with open('./tiki/' + name + '.txt', mode='r') as file:
-            links = file.readlines()
-        with open(name + '.csv', mode='w', newline='', encoding='utf-8-sig') as file_w:
-            for link in links:
-                print(link)
-                list = getItem(link.strip())
-                writer = csv.writer(file_w)
-                for i in list:
-                    print(i)
-                    writer.writerow(i)
+    @staticmethod
+    def iteratingSubList():
+        '''
+        iterating through the 'Sub links' collection and get items information
+        and export data to the corresponding collection
+        :return:
+        '''
+        for x in DataPipeline.importData('Sub links'):
+            logging.debug('Getting item info from {} page {}'.format(x['name'], x['page']))
+            if x['status']:
+                print('Getting item info from {} page {}...'.format(x['name'], x['page']))
+                items_info_list = Item.getItem(x['link'])
+                DataPipeline.exportData(x['name'], items_info_list)
+                DataPipeline.updateStatus('Sub links', x['link'], False)
+                print('Finished getting item info from {} page {}'.format(x['name'], x['page']))
+            else:
+                print('Item info from {} page {} is already exist'.format(x['name'], x['page']))
+            logging.debug('Finished getting item info from {} page {}'.format(x['name'], x['page']))
 
 
 if __name__ == '__main__':
     t1 = time.time()
     loggingInitiate()
-    DataPipeline.exportData('Category links',Category.getCategoryLinkList())
+    DataPipeline.exportData('Category links', Category.getCategoryLinkList())
     SubCategory.iteratingCategoryLinks()
+    Item.iteratingSubList()
     t2 = time.time()
     print(t2 - t1)
