e484ad447094e9cfa62edafb27f4a317b7eef4ae
TienTruong98 <ironlegionrocks@gmail.com>
2019-11-28 11:15:11 +0700

    Update tiki 2.0.py

--- Web Scrapper/tiki/tiki 2.0.py
+++ Web Scrapper/tiki/tiki 2.0.py
@@ -19,58 +19,91 @@ class NoDaemonProcess(multiprocessing.Process):
 class MyPool(multiprocessing.pool.Pool):
     Process = NoDaemonProcess
 
+class ItemScrapper:
+    '''
+    Scrap flow:
+        1. Crawl the display menu page and Get the item URL from the display menu
+        2. Enter the item source page and scrap the item info
+    '''
+    max_attemp = 5
+    @staticmethod
+    def getItemInfo(url):
+        '''
+        get the items information (name, price, rating)
+        :param url: the item source page
+        :return: dict contains item information (name,price,rating,url)
+        '''
+        attempt = 0
+        while attempt < ItemScrapper.max_attemp:
+            try:
+                response = requests.get(url)
+                soup = bs4.BeautifulSoup(response.text, 'lxml')
+                # check redirect url
+                if int(response.headers['Content-Length']) < 1000:
+                    # find the redirect url
+                    regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+                    redirect_url = soup.find_all('script')[0].text
+                    redirect_url = re.findall(regex, redirect_url)[0]
+                    # change the request to the redirect URL
+                    response = requests.get(redirect_url)
+                    soup = bs4.BeautifulSoup(response.text, 'lxml')
 
-def getItemInfo(url):
-    '''
-    get the items information (name, price, rating)
-    :param url: the item source page
-    :return: dict contains item information (name,price,rating,url)
-    '''
-    atemp = 0
-    while atemp<5:
+                name = soup.select_one('.icon-tikinow-26+ span').text
+                price = soup.select_one('#span-price').text
+                rating = soup.find("meta", attrs={"itemprop": "ratingValue"})['content']
+
+                response.close()
+                soup.decompose()
+                return {'name': name, 'price': price, 'rating': rating, 'url': url}
+            except Exception as e:
+                attempt += 1
+        return {'name': None, 'price': None, 'rating': None, 'url': url}
+    @staticmethod
+    def getItemUrls(url):
+        '''
+        scrap the items URL from the display menu page
+        :param url: page URL
+        :return: item_url_list: list of items URl
+        '''
         try:
-            regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-            r = requests.get(url)
-            soup = bs4.BeautifulSoup(r.text, 'lxml')
-            # check redirect url
-            if int(r.headers['Content-Length']) < 1000:
-                x = soup.find_all('script')[0].text
-                x = re.findall(regex, x)[0]
-                r = requests.get(x)
-                soup = bs4.BeautifulSoup(r.text, 'lxml')
-
-            name = soup.select_one('.icon-tikinow-26+ span').text
-            price = soup.select_one('#span-price').text
-            rating = soup.find("meta", attrs={"itemprop": "ratingValue"})['content']
-            r.close()
+            response = requests.get(url)
+            soup = bs4.BeautifulSoup(response.text, 'lxml')
+            item_url_list = [x['href'] for x in soup.select('.product-box-list .product-item > a')]
             soup.decompose()
-            return {'name': name, 'price': price, 'rating': rating, 'url':url}
-        except Exception as e:
-            atemp +=1
-    return {'name': '', 'price': '', 'rating': '', 'url':url}
-
-
-def getItemUrls(url):
-    try:
-        r = requests.get(url)
-        soup = bs4.BeautifulSoup(r.text, 'lxml')
-        product = soup.select('.product-box-list .product-item > a')
-        soup.decompose()
-        r.close()
-        url_list = []
+            response.close()
+            return item_url_list
+        except:
+            pass
+    @staticmethod
+    def scrapItemInfo(url):
+        '''
+        get all information of all items on a display menu
+        :param url: the URL of a display menu page
+        :return: item_info: list of items information
+        '''
+        url_list = ItemScrapper.getItemUrls(url)
         item_info = []
-        for x in product:
-            url_list.append(x['href'])
         try:
-            p = multiprocessing.Pool()
-            item_info = p.map(getItemInfo, url_list)
-            p.close()
-            p.join()
+            pool = multiprocessing.Pool()
+            item_info = pool.map(ItemScrapper.getItemInfo, url_list)
+            pool.close()
+            pool.join()
         except Exception as e:
             print(e)
         return item_info
-    except:
-        pass
+    @staticmethod
+    def crawlPage(url):
+        '''
+        crawl and scrap items information from a list of URLs
+        :param url: list of URLs
+        :return: info: items information
+        '''
+        p = MyPool()
+        info = p.map(ItemScrapper.scrapItemInfo, url)
+        p.close()
+        p.join()
+        return info
+
 
 
 if __name__ == "__main__":
@@ -82,10 +115,7 @@ if __name__ == "__main__":
         'https://tiki.vn/dien-thoai-smartphone/c1795?src=tree',
         'https://tiki.vn/dien-thoai-ban/c8061?src=tree'
     ]
-    p = MyPool()
-    result = p.map(getItemUrls, url)
-    p.close()
-    p.join()
+    result = ItemScrapper.crawlPage(url)
     i = 0
     for x in result:
         for y in x:
