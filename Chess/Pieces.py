6c385e59b1eb9af2033ee42d753b40f9ea3bdadd
TienTruong98 <ironlegionrocks@gmail.com>
2019-12-19 17:24:30 +0700

    Update Pieces.py

--- Chess/Pieces.py
+++ Chess/Pieces.py
@@ -1,4 +1,5 @@
 import pygame
+import itertools
 
 
 class Piece:
@@ -10,25 +11,88 @@ class Piece:
         self.color = color
         self.x = x
         self.y = y
-        if self.color == 'black':
-            self.direction = -1
+
+    def getMove(self, x_range: list = [], y_range: list = [], L_shape=False):
+        if not L_shape:
+            for x, y in zip(x_range, y_range):
+                if chr(x) in self.X_axis and y in self.Y_axis:
+                    yield chr(x), y
         else:
-            self.direction = 1
+            for x, y in self.LShape():
+                if chr(x) in self.X_axis and y in self.Y_axis:
+                    yield chr(x), y
 
-    def moveVertical(self, step):
-        return step, 0
+    def North(self, step):
 
-    def moveHorizontal(self, step):
-        return 0, step
+        if self.color == 'black':
+            y_range = range(self.y - 1, self.y - step - 1, -1)
+        else:
+            y_range = range(self.y + 1, self.y + step + 1)
+        x_range = [ord(self.x)] * len(y_range)
+        return x_range, y_range
 
-    def moveMainDiagonal(self, step):
-        return -step, step
+    def NorthEast(self, step):
+        if self.color == 'black':
+            y_range = range(self.y - 1, self.y - step - 1, -1)
+            x_range = range(ord(self.x) + 1, ord(self.x) + step + 1)
+        else:
+            y_range = range(self.y + 1, self.y + step + 1)
+            x_range = range(ord(self.x) - 1, ord(self.x) - step - 1, -1)
+        return x_range, y_range
 
-    def moveAntiDiagonal(self, step):
-        return step, step
+    def East(self, step):
+        if self.color == 'black':
+            x_range = range(ord(self.x) + 1, ord(self.x) + step + 1)
+        else:
+            x_range = range(ord(self.x) - 1, ord(self.x) - step - 1, -1)
+        y_range = [self.y] * len(x_range)
+        return x_range, y_range
 
-    def moveLShape(self):
-        return [
+    def SouthEast(self, step):
+        if self.color == 'black':
+            y_range = range(self.y + 1, self.y + step + 1)
+            x_range = range(ord(self.x) + 1, ord(self.x) + step + 1)
+        else:
+            y_range = range(self.y - 1, self.y - step - 1, -1)
+            x_range = range(ord(self.x) - 1, ord(self.x) - step - 1, -1)
+        return x_range, y_range
+
+    def South(self, step):
+        if self.color == 'black':
+            y_range = range(self.y + 1, self.y + step + 1)
+        else:
+            y_range = range(self.y - 1, self.y - step - 1, -1)
+        x_range = [ord(self.x)] * len(y_range)
+        return x_range, y_range
+
+    def SouthWest(self, step):
+        if self.color == 'black':
+            y_range = range(self.y + 1, self.y + step + 1)
+            x_range = range(ord(self.x) - 1, ord(self.x) - step - 1, -1)
+        else:
+            y_range = range(self.y - 1, self.y - step - 1, -1)
+            x_range = range(ord(self.x) + 1, ord(self.x) + step + 1)
+        return x_range, y_range
+
+    def West(self, step):
+        if self.color == 'black':
+            x_range = range(ord(self.x) - 1, ord(self.x) - step - 1, -1)
+        else:
+            x_range = range(ord(self.x) + 1, ord(self.x) + step + 1)
+        y_range = [self.y] * len(x_range)
+        return x_range, y_range
+
+    def NorthWest(self, step):
+        if self.color == 'black':
+            y_range = range(self.y - 1, self.y - step - 1, -1)
+            x_range = range(ord(self.x) - 1, ord(self.x) - step - 1, -1)
+        else:
+            y_range = range(self.y + 1, self.y + step + 1)
+            x_range = range(ord(self.x) + 1, ord(self.x) + step + 1)
+        return x_range, y_range
+
+    def LShape(self):
+        moves = [
             (1, 2),
             (2, 1),
             (2, -1),
@@ -38,15 +102,10 @@ class Piece:
             (-2, 1),
             (-1, 2)
         ]
-
-    def getNextPosition(self, moves: tuple):
-        a, b = moves
-        x = chr(ord(self.x) + a)
-        y = self.y + b
-        if x in self.X_axis and y in self.Y_axis:
-            return x, y
-        else:
-            return None, None
+        for index, value in enumerate(moves):
+            x, y = value
+            moves[index] = (ord(self.x) + x, self.y + y)
+        return moves
 
     def setPosition(self, pos):
         self.x, self.y = pos
@@ -59,154 +118,112 @@ class King(Piece):
     def __init__(self, color, x, y):
         Piece.__init__(self, 'king', color, x, y)
         self.img = pygame.image.load('Pieces/' + color + '_king' + '.png').convert()
+        # remove white background
         self.img.set_colorkey((255, 255, 255))
         self.move = {
-            'moveHorizontal': [1, -1],
-            'moveVertical': [1, -1],
-            'moveMainDiagonal': [1, -1],
-            'moveAntiDiagonal': [1, -1]
+            'North': 1,
+            'NorthEast': 1,
+            'East': 1,
+            'SouthEast': 1,
+            'South': 1,
+            'SouthWest': 1,
+            'West': 1,
+            'NorthWest': 1,
         }
-
-    '''
-    def getBasicMove(self):
-        moves = []
-        posible_moves = []
-        moves.append(self.moveHorizontal(self.direction * 1))
-        moves.append(self.moveVertical(self.direction * 1))
-        moves.append(self.moveMainDiagonal(self.direction * 1))
-        moves.append(self.moveAntiDiagonal(self.direction * 1))
-        moves.append(self.moveHorizontal(self.direction * -1))
-        moves.append(self.moveVertical(self.direction * -1))
-        moves.append(self.moveMainDiagonal(self.direction * -1))
-        moves.append(self.moveAntiDiagonal(self.direction * -1))
-        for a, b in moves:
-            x = chr(ord(self.x) + a)
-            y = self.y + b
-            if x in self.X_axis and y in self.Y_axis:
-                posible_moves.append((x, y))
-        return posible_moves
-    '''
 
 
 class Queen(Piece):
     def __init__(self, color, x, y):
         Piece.__init__(self, 'queen', color, x, y)
         self.img = pygame.image.load('Pieces/' + color + '_queen' + '.png').convert()
+        # remove white background
         self.img.set_colorkey((255, 255, 255))
 
-    '''
-    def getBasicMove(self):
-        moves = []
-        posible_moves = []
-        for i in range(1, 8):
-            moves.append(self.moveHorizontal(self.direction * i))
-            moves.append(self.moveVertical(self.direction * i))
-            moves.append(self.moveMainDiagonal(self.direction * i))
-            moves.append(self.moveAntiDiagonal(self.direction * i))
-            moves.append(self.moveHorizontal(self.direction * -i))
-            moves.append(self.moveVertical(self.direction * -i))
-            moves.append(self.moveMainDiagonal(self.direction * -i))
-            moves.append(self.moveAntiDiagonal(self.direction * -i))
-        for a, b in moves:
-            x = chr(ord(self.x) + a)
-            y = self.y + b
-            if x in self.X_axis and y in self.Y_axis:
-                posible_moves.append((x, y))
-        return posible_moves
-    '''
+        self.move = {
+            'North': 7,
+            'NorthEast': 7,
+            'East': 7,
+            'SouthEast': 7,
+            'South': 7,
+            'SouthWest': 7,
+            'West': 7,
+            'NorthWest': 7,
+        }
 
 
 class Rook(Piece):
     def __init__(self, color, x, y):
-        Piece.__init__(self, 'rock', color, x, y)
+        Piece.__init__(self, 'rook', color, x, y)
         self.img = pygame.image.load('Pieces/' + color + '_rook' + '.png').convert()
+        # remove white background
         self.img.set_colorkey((255, 255, 255))
 
-    '''
-    def getBasicMove(self):
-        moves = []
-        posible_moves = []
-        for i in range(1, 8):
-            moves.append(self.moveHorizontal(self.direction * i))
-            moves.append(self.moveVertical(self.direction * i))
-            moves.append(self.moveHorizontal(self.direction * -i))
-            moves.append(self.moveVertical(self.direction * -i))
-        for a, b in moves:
-            x = chr(ord(self.x) + a)
-            y = self.y + b
-            if x in self.X_axis and y in self.Y_axis:
-                posible_moves.append((x, y))
-        return posible_moves
-    '''
+        self.move = {
+            'North': 7,
+            'East': 7,
+            'South': 7,
+            'West': 7,
+        }
 
 
 class Knight(Piece):
     def __init__(self, color, x, y):
         Piece.__init__(self, 'knight', color, x, y)
         self.img = pygame.image.load('Pieces/' + color + '_knight' + '.png').convert()
+        # remove white background
         self.img.set_colorkey((255, 255, 255))
 
-    '''
-    def getBasicMove(self):
-        moves = []
-        posible_moves = []
-        moves.extend(self.moveLShape())
-        for a, b in moves:
-            x = chr(ord(self.x) + a)
-            y = self.y + b
-            if x in self.X_axis and y in self.Y_axis:
-                posible_moves.append((x, y))
-        return posible_moves
-    '''
+        self.move = {
+            'LShape': 0
+        }
 
 
 class Bishop(Piece):
     def __init__(self, color, x, y):
         Piece.__init__(self, 'bishop', color, x, y)
         self.img = pygame.image.load('Pieces/' + color + '_bishop' + '.png').convert()
+        # remove white background
         self.img.set_colorkey((255, 255, 255))
 
-    '''
-    def getBasicMove(self):
-        moves = []
-        posible_moves = []
-        for i in range(1, 8):
-            moves.append(self.moveMainDiagonal(self.direction * i))
-            moves.append(self.moveAntiDiagonal(self.direction * i))
-            moves.append(self.moveMainDiagonal(self.direction * -i))
-            moves.append(self.moveAntiDiagonal(self.direction * -i))
-        for a, b in moves:
-            x = chr(ord(self.x) + a)
-            y = self.y + b
-            if x in self.X_axis and y in self.Y_axis:
-                posible_moves.append((x, y))
-        return posible_moves
-    '''
+        self.move = {
+            'NorthEast': 7,
+            'SouthEast': 7,
+            'SouthWest': 7,
+            'NorthWest': 7,
+        }
 
 
 class Pawn(Piece):
     def __init__(self, color, x, y):
         Piece.__init__(self, 'pawn', color, x, y)
         self.img = pygame.image.load('Pieces/' + color + '_pawn' + '.png').convert()
+        # remove white background
         self.img.set_colorkey((255, 255, 255))
+        self.first_move = True
 
-    '''
-    def getBasicMove(self):
-        moves = []
-        posible_moves = []
-        moves.append(self.moveHorizontal(self.direction * 1))
-        for a, b in moves:
-            x = chr(ord(self.x) + a)
-            y = self.y + b
-            if x in self.X_axis and y in self.Y_axis:
-                posible_moves.append((x, y))
-        return posible_moves
-    '''
+    @property
+    def move(self):
+        if self.first_move:
+            return {
+                'North': 2,
+            }
+        else:
+            return {
+                'North': 1,
+            }
 
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((0, 0))
-    knight = Queen('white', 'A', 1)
-    print(knight)
-    print(knight.getBasicMove())
+    knight = Queen('white', 'E', 1)
+    for function, step in knight.move.items():
+        if function == 'LShape':
+            for x in knight.getMove(L_shape=True):
+                print(x)
+        else:
+            f = getattr(knight, function)
+            range_x, range_y = f(step)
+            print(f.__name__)
+            for x in knight.getMove(range_x, range_y):
+                print(x)
