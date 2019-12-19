901a0952088dc1a443851e713986474ad4eba2b4
TienTruong98 <ironlegionrocks@gmail.com>
2019-12-19 21:04:40 +0700

    add endGame conditons
    Display the winner and block all mouse actions

--- Chess/Chess.py
+++ Chess/Chess.py
@@ -4,6 +4,14 @@ import Chess.Game as Game
 import pygame
 
 
+def endGame():
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    text = font.render(f'{game.winner} win!!!!!', True, (255, 255, 255), (0, 0, 0))
+    textRect = text.get_rect()
+    textRect.center = (600 // 2, 600 // 2)
+    screen.blit(text, textRect)
+
+
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((600, 600))
@@ -17,10 +25,12 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                screen_pos = pygame.mouse.get_pos()
-                game.select(screen_pos)
+                if game.status:
+                    screen_pos = pygame.mouse.get_pos()
+                    game.select(screen_pos)
         screen.fill((0, 0, 0))
         game.board.draw(screen)
         game.board.drawSquare(screen, pygame.mouse.get_pos())
-
+        if not game.status:
+            endGame()
         pygame.display.update()
