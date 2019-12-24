da3a79da14657cc8f0a5ee58c27a9d75a453dd00
TienTruong98 <ironlegionrocks@gmail.com>
2019-12-24 19:02:58 +0700

    Install bot move

--- Chess/chess_v2.py
+++ Chess/chess_v2.py
@@ -1,6 +1,15 @@
 import pygame
 import Chess.Game_v2 as Game
 import Chess.Board_v2 as Board
+import Chess.Player_v2 as Player
+
+
+def endGame():
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    text = font.render(f'{game.winner} win!!!!!', True, (255, 255, 255), (0, 0, 0))
+    textRect = text.get_rect()
+    textRect.center = (600 // 2, 600 // 2)
+    screen.blit(text, textRect)
 
 
 def drawBoard(board: Board.Board, board_pos: tuple):
@@ -9,6 +18,27 @@ def drawBoard(board: Board.Board, board_pos: tuple):
         if square.occupant is not None:
             x, y = square.left_up_corner
             screen.blit(square.occupant.img, (x, y))
+
+
+def botMove():
+    player = getattr(game, game.turn)
+    if type(player) is Player.Bot:
+        old, new = player.chooseMove()
+        print(old, new)
+        game.moving_piece = old.occupant
+        game.checkPawn()
+        game.writeHistory(old, new)
+        old.occupant = None
+        game.changeTurn()
+        drawBoard(game.board, (0, 0))
+        screen.blit(game.moving_piece.img, new.left_up_corner)
+        new.occupant = game.moving_piece
+        game.moving_piece.pos = new.pos
+        game.moving_piece = None
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -21,7 +51,7 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Chess')
 
-    game = Game.Game(board_size)
+    game = Game.Game(board_size, player={'black': 'bot', 'white': 'human'})
     game.setUp()
     running = True
     while running:
@@ -37,4 +67,9 @@ if __name__ == '__main__':
         # draw moving pieces
         if game.moving_piece is not None:
             screen.blit(game.moving_piece.img, pygame.mouse.get_pos())
+        if not game.status:
+            endGame()
         pygame.display.update()
+        if game.status:
+            botMove()
+
