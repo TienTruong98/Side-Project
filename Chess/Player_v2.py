1a93095b9b7896f107d26761d245c7ce89404cab
TienTruong98 <ironlegionrocks@gmail.com>
2019-12-24 19:03:53 +0700

    Update Player_v2.py

--- Chess/Player_v2.py
+++ Chess/Player_v2.py
@@ -7,77 +7,20 @@ class Players:
         self.color = color
         self.pieces = pieces
         self.score = 0
-        self.history = []
         self.board = Board.Board()
-        self.moving_piece = None  # if player click one piece
         self.old_square = None  # if player click then remember the old square
+        self.moving_piece = None  # if player is moving a piece
         self.opponent = None
 
-
-class Human(Players):
-    def __init__(self, color):
-        Players.__init__(self, color, [])
-
-    def click(self, pos: tuple) -> Pieces:
-        # if player click a piece then remove that piece on the board
-        # and player remember the selected piece and the old square
-        # return the moving piece
-        square = self.board.findSquare(pos)
-        if square.occupant is None or square.occupant.color != self.color:
-            return
-        self.moving_piece = square.occupant
-        self.old_square = square
-        square.occupant = None
-        return self.moving_piece
-
-    def drop(self, pos):
-        has_move = False  # check if the move is legal
-        next_square = self.board.findSquare(pos)
-        # check if the player click on the old square again
-        if next_square == self.old_square:
-            self.old_square.occupant = self.moving_piece
-            self.old_square = None
-            self.moving_piece = None
-            return has_move
-
-        else:
-            has_move = self.eatPieces(next_square)
-            return has_move
-
-    def eatPieces(self, next_square):
-        has_move = True
-        # check if the next square is not the same color
-        if next_square.occupant not in self.pieces:
-            possible_move = self.getPossibleMove(self.old_square, self.color)
-            for x in possible_move:
-                print(x, end=' ')
-            print()
-            # check if the next square is legal move
-            if next_square in possible_move:
-                next_square.occupant = self.moving_piece
-                self.moving_piece.pos = next_square.pos
-                has_move = True
-            else:
-                has_move = False
-        else:
-            has_move = False
-        if not has_move:
-            # return the piece to old square
-            self.old_square.occupant = self.moving_piece
-
-        self.old_square = None
-        self.moving_piece = None
-        return has_move
-
-    def getPossibleMove(self, square, turn):
+    def getPossibleMove(self):
         def KnightMove():
             for pos in occupant.getMove(L_shape=True):
+                # get all possible L_shape Move
                 square = self.board.findSquare(pos)
                 if square.occupant is None:
                     possible_moves.append(square)
-                elif square.occupant.color != turn:
+                elif square.occupant not in self.pieces:
                     possible_eats.append(square)
-
         def getMove(action, condition):
             def pawnMoveCondition(moves):
                 is_block = False
@@ -99,14 +42,14 @@ class Human(Players):
                     if square.occupant is None:
                         is_block = True
                     else:
-                        if square.occupant.color == turn:
+                        if square.occupant in self.pieces:
                             is_block = True
                         else:
                             possible_eats.append(square)
                             is_block = True
 
             def piecesMoveCondition(moves):
-                is_block = False
+                is_block = False  # check if that direction is blocked by a friendly piece
                 for pos in moves:
                     if is_block:
                         break
@@ -114,24 +57,22 @@ class Human(Players):
                     if square.occupant is None:
                         possible_moves.append(square)
                     else:
-                        if square.occupant.color == turn:
+                        if square.occupant in self.pieces:
                             is_block = True
                         else:
                             possible_eats.append(square)
                             is_block = True
 
-            # get possible square in piece's directions
+            # get possible square in piece's movement
             for direction, step in getattr(occupant, action).items():
-                f = getattr(occupant, direction)
-                range_x, range_y = f(step)
-                moves = occupant.getMove(range_x, range_y)
-                # choose suitable square
+                moves = occupant.getMove(direction, step)  # get possible move to the 'direction'
+                # choose suitable square based on their movement condition
                 choose = eval(condition)
                 choose(moves)
 
         occupant: Pieces = self.moving_piece
-        possible_moves = []
-        possible_eats = []
+        possible_moves = []  # posible move
+        possible_eats = []  # possible opponet pieces to eat
         if type(occupant) is Pieces.Knight:
             KnightMove()
         elif type(occupant) is Pieces.Pawn:
@@ -142,7 +83,57 @@ class Human(Players):
         possible_moves.extend(possible_eats)
         return possible_moves
 
+    def removePiece(self, piece):
+        for index, p in enumerate(self.pieces):
+            if p == piece:
+                self.pieces.pop(index)
+                return
+class Human(Players):
+    def __init__(self, color):
+        Players.__init__(self, color, [])
+
+    def click(self, pos: tuple) -> Pieces:
+        # if player click a piece then remove that piece on the board
+        # and player remember the selected piece and the old square
+        # return the moving piece
+        square = self.board.findSquare(pos)
+        if square.occupant is None or square.occupant not in self.pieces:
+            return None
+        self.moving_piece = square.occupant
+        self.old_square = square
+        square.occupant = None
+        return self.moving_piece
+
+    def drop(self, pos):
+        # decide if the pos is legal
+        # true: return the old square and the new square
+        # false: return the old square
+        next_square = self.board.findSquare(pos)
+        # check if the player click on the old square again
+        if next_square == self.old_square:
+            self.old_square = None
+            return next_square, next_square
+        else:
+            # check if the next square is not the same color
+            if next_square.occupant not in self.pieces:
+                possible_move = self.getPossibleMove()
+                # check if the next square is legal move
+                if next_square in possible_move:
+                    return self.old_square, next_square
+            return self.old_square, self.old_square
+import random
 
 class Bot(Players):
     def __init__(self, color):
         Players.__init__(self, color, [])
+    def chooseMove(self):
+        possible_move = []
+        while len(possible_move)==0:
+            self.moving_piece = random.choice(self.pieces)
+            possible_move = self.getPossibleMove()
+        move = random.choice(possible_move)
+        old_square = self.board.findSquare(self.moving_piece.pos)
+        new_square = move
+        self.moving_piece = None
+        return old_square, new_square
+
